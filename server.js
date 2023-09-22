fabd6574fccdc3a83f8d183c1209b599619c649d
jj <saoitjensen@gmail.com>
2023-09-22 17:43:10 -0600

    base server logic

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Server is live!'));
+});
