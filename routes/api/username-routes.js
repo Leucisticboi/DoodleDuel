b549061361dcecb99be161d8e552eb307857ac12
jj <saoitjensen@gmail.com>
2023-09-26 21:29:13 -0600

    routes added

---
+++ routes/api/username-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User, Stats } = require('../../models');
+
+router.get('/:username', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.body.username, {
+            include: [{ model: Stats }],
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user with that username.'});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
