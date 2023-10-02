46b3febf3e45d19f603a07345268e22e7402da69
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-10-02 17:16:51 -0600

    Updated connection info for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // If JAWSDB_URL is set, use that for the connection
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use separate environment variables
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        port: 3306
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: process.env.DB_PORT
     }
-);
+  );
+}
 
 module.exports = sequelize;
