9d681747ad5ed55c3bbddb441022bea5b11f54e2
Cardon Hickman <cardonhickman@gmail.com>
2023-10-02 21:09:09 -0600

    changed homeroutes to match recent merges

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,25 +1,15 @@
 const router = require('express').Router();
+const withAuth = require('../util/auth');
 const Prompt = require('../models/Prompts');
 
-router.get('/', async (req, res) => {
+//home
+router.get('/', withAuth, async (req, res) => {
+    console.log('hit home');
     try {
         res.render('homepage', {
             layout: 'main', // Specify the layout here
             logged_in: req.session.logged_in,
             user: req.user,
-        });
-        console.log("req.user:", req.user);
-    } catch (error) {
-        console.error('Error fetching prompts:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-router.get('/duel', async (req, res) => {
-    try {
-        res.render('duel', {
-            layout: 'main', // Specify the layout here
-            logged_in: req.session.logged_in,
         });
         console.log("req.user:", req.user);
     } catch (error) {
@@ -42,6 +32,21 @@ router.get('/signup', (req, res) => {
         return;
     }
     res.render('signup');
+});
+
+//duel
+router.get('/duel', withAuth, async (req, res) => {
+    try {
+        res.render('duel', {
+            layout: 'main', // Specify the layout here
+            logged_in: req.session.logged_in,
+            user: req.user,
+        });
+        console.log("req.user:", req.user);
+    } catch (error) {
+        console.error('Error fetching prompts:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.get('/vote/:id', async (req, res) => {
